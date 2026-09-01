3ee69b966ae1c754c64a38612660ee7e190356cd
Jaybranch38566 <jbranch957@boyslatin.net>
2026-09-01 14:24:49 +0000

    Notes

---
+++ Datatypes.py
@@ -0,0 +1,5 @@
+ String- #characters that are wrapped within quotes.
+#characters can be letters,numbers,and symbols.
+Integers-A whole number that are generally used for calculations without a decimal or symbols.
+Float-A number with a decimal.
+Boolean-The Data type is a value that can only be True or Fasle.
