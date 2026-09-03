819ba2457a36d9050d430cf84718bd0eebafd7a6
Jaybranch38566 <jbranch957@boyslatin.net>
2026-09-03 14:20:07 +0000

    Variable Activity Assignment

--- Datatypes.py
+++ Datatypes.py
@@ -3,3 +3,4 @@
 Integers-A whole number that are generally used for calculations without a decimal or symbols.
 Float-A number with a decimal.
 Boolean-The Data type is a value that can only be True or Fasle.
+Variables-Are not data types but rather are a type of information.
---
+++ unit 1/Variable Activit.py
@@ -0,0 +1,10 @@
+Variables-A container that stores data.You can name your container almost anything you want,so long as it is descriptive.
+#Variable Naming Rules
+1.A variable must start with letter and cannot start with a number
+#2. A variable can be mixed a of letters and numbers BUT not symbols
+#3. A variable CANNOT be a Python keyword
+Number of apples in a supermarket-Apple count Data type:Integer
+Students in a math "class" Integer
+Insurance for trip- False
+Bronze and gold membership for a gym-40
+Price of 3 drinks- Variable,Float
