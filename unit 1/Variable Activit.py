1cc8f01d0cd5aacbb5dfb56b2cbc9f08b24a679c
Jaybranch38566 <jbranch957@boyslatin.net>
2026-09-04 14:31:32 +0000

    AIBasics.txt Assignment

--- unit 1/Variable Activit.py
+++ unit 1/Variable Activit.py
@@ -7,4 +7,7 @@ Number of apples in a supermarket-Apple count Data type:Integer
 Students in a math "class" Integer
 Insurance for trip- False
 Bronze and gold membership for a gym-40
-Price of 3 drinks- Variable,Float
+Price of 3 drinks- Variable,Floa
+We use naming conventions because we CANNOT have spaces in variable names
+Camel case-when creating variable names with mulitple words,every new word begins with a capital letter
+the reason why its called camel case is because every new word represents a hump.
