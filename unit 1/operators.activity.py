2837f958015f9575aab122890cae5595782fb227
Jaybranch38566 <jbranch957@boyslatin.net>
2026-09-15 14:31:38 +0000

    Operators.activity Assignment

---
+++ unit 1/operators.activity.py
@@ -0,0 +1,11 @@
+print (200==100) 
+#false
+
+print(15<18)
+#true
+
+print("Coding 1" == "coding1") 
+#false
+
+print ("0" != 0)
+#True
--- unit 1/operators.py
+++ unit 1/operators.py
@@ -1,10 +1,23 @@
 #Assingment Operators-Exclusivley used to assign values to variables.
 #think key/value pairings.
+
 # we use a single equal sign to represent the assignment operator.
 #Arithmetic Operators-Used on numerical data types to peform calculations.
+
 #Integers (whole numbers) and floats (decimal numbers)
 #print is a function that lets us show code in the terminal.
 print  (3+3) #addition operator
 print (3.9-3)#subtraction operator
 print(12/3) #division operator
 print(3*4) #multiplication operator
+
+#comparison  operators-set of symbols used to assets if data is the same or diffrent and how they differ.
+print (10>1) #greater than operator
+print (2000<100) #less than operator
+
+# 2 equal signs compare if something is the same 
+#logical operators-compares 2 conditions to check if they are true or false conditions=other operators instead of symbols,we represent these with words: and,or,and not
+
+#and-checks if 2 conditions are true.if yes the final result true
+print (3>1) and 100>50 #this would come out to be true
+#or-checks if only one conditon is true.if yes, the final result will be true
