b0821b8307dea7ea9de00f21153187a331c5b0be
Jaybranch38566 <jbranch957@boyslatin.net>
2026-09-22 14:34:11 +0000

    built in fuctions assigment

---
+++ Unit 2/builtinfunctions.py
@@ -0,0 +1,26 @@
+#Function- simply put; a code block of 
+#instructions for computers to follow.
+#Built- in Function - a code block of instruction for a computer to follow 
+#that was already written for us.
+#PRE-WRITTEN CODE INSTRUCTIONS
+#input () a built in function that allows a user to
+# type in data into the terminal. the data that is passed 
+#in the will always be treated as a string data type.
+#print () a built in function that allows a user to 
+#print data out of the terminal.
+#w3 schools
+#Data casting functions
+#these are built in (pre written) functions
+#that change data types from one  form into another
+str()#- this datacasting function allows you to change any data type into a string
+year=1906
+print ("This event took place in")+year 
+#string concatenation-combining string data types
+#with one another.We use the plus sign (+) operator
+#to combine strings together.
+
+#int()-whole numbers used for calculations ,and works without decimals and symbols
+float ()-numbers #with decimals
+
+#example-int 
+#example-float(3.9-3) 
